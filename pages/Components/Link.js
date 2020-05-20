c4a7ab97ccb86acefcb2cf3a8986d67551954570
wilson597 <wilkin850@settour.com.tw>
2020-05-20 15:26:32 +0800

    Feed data to the components using getInitialProps

---
+++ pages/Components/Link.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+// Using the router to detect the active link
+export default ({ href, children }) => {
+    const router = useRouter();
+    let className = children.props.className || '';
+    if (router.pathname === href) {
+        className = `${className} selected`;
+    }
+
+    return (
+        <Link href={href}>
+            {React.cloneElement(children, { className })}
+        </Link>
+    )
+}
+
